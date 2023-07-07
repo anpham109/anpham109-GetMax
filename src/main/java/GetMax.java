b280bbdbaeb26881967de8c64db525c1f10cae5d
Andy Pham <anpham-work@protonmail.com>
2023-07-07 12:33:05 +0000

    finished the lab

--- src/main/java/GetMax.java
+++ src/main/java/GetMax.java
@@ -7,6 +7,12 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+        int result = arr[0];
+        for (int var : arr) {
+            if (result < var){
+                result = var;
+            }
+        }
+        return result;
     }
 }
